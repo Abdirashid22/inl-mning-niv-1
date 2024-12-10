b6375d9728af6c3bb314e15945fab9cc2f39caf0
absa07002 <absa07002@DESKTOP-TAGHD5A>
2024-12-10 14:21:54 +0100

    Add project files.

---
+++ inlämning loopar 2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Inlämning_nivå_1
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <=10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+           
+
+
+
+    }
+
+
+
+
+
+}
